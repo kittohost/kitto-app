08ab7cbb4784a922d6cb1e02287f53273520f60c
kittohost <riki2293000@gmail.com>
2025-05-05 12:06:09 +0000

    Add initial App component and styles

---
+++ src/pages/StorePage.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './StorePage.css';
+
+const StorePage = () => {
+  return (
+    <div className="store-page">
+      <h1>Our Hosting Boxes</h1>
+      <div className="product-grid">
+        {/* תכף נוסיף פה מוצרים */}
+      </div>
+    </div>
+  );
+};
+
+export default StorePage;
